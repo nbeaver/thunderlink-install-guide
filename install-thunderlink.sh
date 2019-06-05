6ef7f36de6013d02a8bb94c5acd084cdb5efe4b9
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2019-06-05 13:36:49 -0500

    Snapshot.

--- install-thunderlink.sh
+++ install-thunderlink.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env sh
-
-desktop-file-install thunderbird-thunderlink-handler.desktop
-gio mime x-scheme-handler/thunderlink thunderbird-thunderlink-handler.desktop
-XDG_UTILS_DEBUG_LEVEL=2 xdg-mime default 'thunderbird-thunderlink-handler.desktop' 'x-scheme-handler/thunderlink'
+apps="${XDG_DATA_HOME:-$HOME/.local/share}/applications"
+if ! test -d "${apps}"
+then
+    mkdir -p "${apps}"
+fi
+desktop-file-install --dir="${apps}" thunderbird-thunderlink-handler.desktop
+xdg-mime default 'thunderbird-thunderlink-handler.desktop' 'x-scheme-handler/thunderlink'
+if command gio 2> /dev/null
+then
+    gio mime x-scheme-handler/thunderlink thunderbird-thunderlink-handler.desktop
+fi
