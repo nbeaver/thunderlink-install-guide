e560ac30bcf816bc002f02015d63e732bfaadcc0
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2019-06-05 13:48:30 -0500

    Distinguish between path and filename.

--- install-thunderlink.sh
+++ install-thunderlink.sh
@@ -1,15 +1,16 @@
 #! /usr/bin/env sh
 SCHEME=thunderlink
 MIMETYPE="x-scheme-handler/${SCHEME}"
-DESKTOP_FILE=thunderbird-thunderlink-handler.desktop
+DESKTOP_FILEPATH=thunderbird-thunderlink-handler.desktop
+DESKTOP_FILENAME=$(basename "${DESKTOP_FILEPATH}")
 apps="${XDG_DATA_HOME:-$HOME/.local/share}/applications"
 if ! test -d "${apps}"
 then
     mkdir -p "${apps}"
 fi
-desktop-file-install --dir="${apps}" "${DESKTOP_FILE}"
-xdg-mime default "${DESKTOP_FILE}" "${MIMETYPE}"
+desktop-file-install --dir="${apps}" "${DESKTOP_FILEPATH}"
+xdg-mime default "${DESKTOP_FILENAME}" "${MIMETYPE}"
 if command gio 2> /dev/null
 then
-    gio mime "${MIMETYPE}" "${DESKTOP_FILE}"
+    gio mime "${MIMETYPE}" "${DESKTOP_FILENAME}"
 fi
