32447e7b72619cad6dd5d14b7e083d76d45a9abe
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2019-06-05 13:40:07 -0500

    Parametrize.

--- install-thunderlink.sh
+++ install-thunderlink.sh
@@ -1,12 +1,15 @@
 #! /usr/bin/env sh
+SCHEME=thunderlink
+MIMETYPE="x-scheme-handler/${SCHEME}"
+DESKTOP_FILE=thunderbird-thunderlink-handler.desktop
 apps="${XDG_DATA_HOME:-$HOME/.local/share}/applications"
 if ! test -d "${apps}"
 then
     mkdir -p "${apps}"
 fi
-desktop-file-install --dir="${apps}" thunderbird-thunderlink-handler.desktop
-xdg-mime default 'thunderbird-thunderlink-handler.desktop' 'x-scheme-handler/thunderlink'
+desktop-file-install --dir="${apps}" "${DESKTOP_FILE}"
+xdg-mime default "${DESKTOP_FILE}" "${MIMETYPE}"
 if command gio 2> /dev/null
 then
-    gio mime x-scheme-handler/thunderlink thunderbird-thunderlink-handler.desktop
+    gio mime "${MIMETYPE}" "${DESKTOP_FILE}"
 fi
